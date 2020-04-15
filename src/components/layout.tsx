30d331c452f4e70aaddb19433e9fe1d9b382113f
tmaulon <tmaulon@betomorrow.com>
2020-04-15 13:53:54 +0200

    initialize new Gatsby project with typescript

---
+++ src/components/layout.tsx
@@ -0,0 +1,32 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React, { ReactNode } from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import { Header } from "./header"
+import { Footer } from "./footer"
+
+export const Layout = ({ children }: { children: ReactNode }) => {
+	const data = useStaticQuery(graphql`
+		query SiteTitleQuery {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+	`)
+
+	return (
+		<>
+			<Header siteTitle={data.site.siteMetadata.title} />
+			<main>{children}</main>
+			<Footer />
+		</>
+	)
+}
