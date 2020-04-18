45f4655addb431755d7c93ad02e78a205d215dff
tmaulon <thomas.maulon@gmail.com>
2020-04-18 15:18:35 +0200

    begin query data for presentation section in index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,115 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import { Layout } from "../components/layout"
 import { Image } from "../components/image"
 import { SEO } from "../components/seo"
+import { I18nString, I18nPicture, I18nTextCta } from "../utils/i18n"
+import { PresentationSection } from "../components/presentation-section"
 
-const IndexPage = () => (
-	<Layout>
-		<SEO title="Home" />
-		<h1>Hi people</h1>
-		<p>Welcome to your new Gatsby site.</p>
-		<p>Now go build something great.</p>
-		<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+export interface IPresentationSection {
+	sectionTitle: I18nString
+	sectionDescription: I18nString
+	mockupsPicture: I18nPicture
+	desktopBackgroundImage: I18nPicture
+	mobileBackgroundImage: I18nPicture
+}
+
+export interface ICommonComponents {
+	requestInviteCta: I18nTextCta
+}
+interface IHomePage {
+	presentationSection: IPresentationSection
+}
+interface IDataJson {
+	homePage: IHomePage
+	commonComponents: ICommonComponents
+}
+interface IData {
+	dataJson: IDataJson
+}
+
+const IndexPage = () => {
+	const data: IData = useStaticQuery(graphql`
+		query MyQuery {
+			dataJson {
+				homePage {
+					presentationSection {
+						desktopBackgroundImage {
+							src
+							alt {
+								en
+								fr
+							}
+						}
+						sectionDescription {
+							fr
+							en
+						}
+						sectionTitle {
+							fr
+							en
+						}
+						mockupsPicture {
+							alt {
+								en
+								fr
+							}
+							src
+						}
+						mobileBackgroundImage {
+							alt {
+								en
+								fr
+							}
+							src
+						}
+					}
+				}
+				commonComponents {
+					requestInviteCta {
+						link
+						text {
+							en
+							fr
+						}
+						title {
+							en
+							fr
+						}
+					}
+				}
+			}
+		}
+	`)
+
+	const { homePage, commonComponents } = data.dataJson
+	const { presentationSection } = homePage
+	const {
+		sectionTitle,
+		sectionDescription,
+		mockupsPicture,
+		desktopBackgroundImage,
+		mobileBackgroundImage,
+	} = presentationSection
+	const { requestInviteCta } = commonComponents
+
+	return (
+		<Layout>
+			<SEO title="Home" />
+			<PresentationSection
+				sectionTitle={sectionTitle}
+				sectionDescription={sectionDescription}
+				mockupsPicture={mockupsPicture}
+				desktopBackgroundImage={desktopBackgroundImage}
+				mobileBackgroundImage={mobileBackgroundImage}
+				requestInviteCta={requestInviteCta}
+			/>
+			{/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
 			<Image />
-		</div>
-		<Link to="/page-2/">Go to page 2</Link>
-	</Layout>
-)
+		</div> */}
+		</Layout>
+	)
+}
 
 export default IndexPage
