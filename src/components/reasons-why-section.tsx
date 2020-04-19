009edd6287b73cebb8728b458dfc2bfbffdff108
tmaulon <thomas.maulon@gmail.com>
2020-04-19 19:19:22 +0200

    imprve reasons why section

--- src/components/reasons-why-section.tsx
+++ src/components/reasons-why-section.tsx
@@ -4,29 +4,31 @@ import { Typography } from "../utils/typography"
 import { Colors } from "../utils/colors"
 import styled from "styled-components"
 import { Wrapper } from "./container"
+import { IReasonsWhyEdges } from "../pages/index"
+import Img from "gatsby-image"
 
 export const ReasonsWhySection = ({
 	sectionTitle,
 	sectionDescription,
-	theReasonsWhy,
+	allReasonsWhyJson,
 }: {
 	sectionTitle: I18nString
 	sectionDescription: I18nString
-	theReasonsWhy: string[]
+	allReasonsWhyJson: IReasonsWhyEdges
 }) => (
 	<StyledSection>
 		<StyledContainer>
 			<Title>{sectionTitle.en}</Title>
 			<Text>{sectionDescription.en}</Text>
-			<ul>
-				{theReasonsWhy.map((r, index) => {
-					return (
-						<li key={`${r}-${index}`}>
-							<p>{r}</p>
-						</li>
-					)
-				})}
-			</ul>
+			<ReasonsList>
+				{allReasonsWhyJson.edges.map(({ node: reason }, index) => (
+					<Reason key={`${reason.id}-${index}`}>
+						<ReasonIcon fluid={reason.picture.src.childImageSharp.fluid} alt={reason.picture.alt.en} />
+						<p>{reason.title.en}</p>
+						<p>{reason.description.en}</p>
+					</Reason>
+				))}
+			</ReasonsList>
 		</StyledContainer>
 	</StyledSection>
 )
@@ -54,4 +56,23 @@ const Text = styled.p`
 	font-size: ${Typography.fontSize.main};
 	text-align: center;
 	color: ${Colors.neutral.grayishBlue};
+`
+const ReasonsList = styled.ul`
+	list-style-type: none;
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: repeat(auto, 1fr);
+	@media screen and (min-width: 768px) {
+		grid-template-columns: repeat(4, 1fr);
+	}
+`
+const Reason = styled.li`
+	grid-column: span 1;
+	grid-row: span 1;
+	text-align: center;
+`
+const ReasonIcon = styled(Img)`
+	max-width: 75px;
+	height: auto;
+	margin: 0 auto;
 `
