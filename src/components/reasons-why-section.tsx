592d085b6088c92febac22b6d652662759b8ad07
tmaulon <thomas.maulon@gmail.com>
2020-04-20 10:38:40 +0200

    implements all reasons why  in the section

--- src/components/reasons-why-section.tsx
+++ src/components/reasons-why-section.tsx
@@ -19,13 +19,13 @@ export const ReasonsWhySection = ({
 	<StyledSection>
 		<StyledContainer>
 			<Title>{sectionTitle.en}</Title>
-			<Text>{sectionDescription.en}</Text>
+			<Text dangerouslySetInnerHTML={{ __html: sectionDescription.en }} />
 			<ReasonsList>
 				{allReasonsWhyJson.edges.map(({ node: reason }, index) => (
 					<Reason key={`${reason.id}-${index}`}>
 						<ReasonIcon fluid={reason.picture.src.childImageSharp.fluid} alt={reason.picture.alt.en} />
-						<p>{reason.title.en}</p>
-						<p>{reason.description.en}</p>
+						<ReasonTitle>{reason.title.en}</ReasonTitle>
+						<ReasonDescription>{reason.description.en}</ReasonDescription>
 					</Reason>
 				))}
 			</ReasonsList>
@@ -36,7 +36,7 @@ const StyledSection = styled.section`
 	position: relative;
 	z-index: 0;
 	padding: 100px 0;
-	background-color: ${Colors.neutral.veryLightGray};
+	background-color: ${Colors.neutral.lightGrayishBlue};
 `
 const StyledContainer = styled(Wrapper)`
 	position: relative;
@@ -49,6 +49,9 @@ const Title = styled.h2`
 	text-align: center;
 	color: ${Colors.primary.darkBlue};
 	margin: 0 0 20px;
+	@media screen and (min-width: 768px) {
+		text-align: left;
+	}
 `
 const Text = styled.p`
 	font-family: ${Typography.fontFamily.mainSansSerif};
@@ -56,12 +59,19 @@ const Text = styled.p`
 	font-size: ${Typography.fontSize.main};
 	text-align: center;
 	color: ${Colors.neutral.grayishBlue};
+	margin-bottom: 60px;
+	@media screen and (min-width: 768px) {
+		text-align: left;
+	}
 `
 const ReasonsList = styled.ul`
 	list-style-type: none;
 	display: grid;
 	grid-template-columns: 1fr;
 	grid-template-rows: repeat(auto, 1fr);
+	padding: 0;
+	margin: 0;
+	grid-gap: 40px;
 	@media screen and (min-width: 768px) {
 		grid-template-columns: repeat(4, 1fr);
 	}
@@ -70,9 +80,37 @@ const Reason = styled.li`
 	grid-column: span 1;
 	grid-row: span 1;
 	text-align: center;
+	padding: 0;
+	margin: 0;
 `
 const ReasonIcon = styled(Img)`
 	max-width: 75px;
 	height: auto;
-	margin: 0 auto;
+	margin: 0 auto 30px;
+
+	@media screen and (min-width: 768px) {
+		margin: 0 0 30px;
+	}
+`
+const ReasonTitle = styled.h3`
+	font-family: ${Typography.fontFamily.mainSansSerif};
+	font-weight: ${Typography.fontWeight.regular};
+	font-size: ${Typography.fontSize.subtitles};
+	text-align: center;
+	color: ${Colors.primary.darkBlue};
+	margin: 0 auto 20px;
+	@media screen and (min-width: 768px) {
+		text-align: left;
+	}
+`
+const ReasonDescription = styled.p`
+	font-family: ${Typography.fontFamily.mainSansSerif};
+	font-weight: ${Typography.fontWeight.regular};
+	font-size: ${Typography.fontSize.main};
+	text-align: center;
+	color: ${Colors.neutral.grayishBlue};
+	margin: 0 auto 40px;
+	@media screen and (min-width: 768px) {
+		text-align: left;
+	}
 `
