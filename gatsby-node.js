8a2f94ec78e85cca4626e7d9d08bfb6318808948
tmaulon <thomas.maulon@gmail.com>
2020-04-20 15:51:51 +0200

    begin and try to implement  exports.createSchemaCustomization in gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,40 @@
  */
 
 // You can delete this file if you're not using it
+// exports.createSchemaCustomization = ({ actions }) => {
+// 	const { createTypes } = actions
+// 	const typeDefs = [
+// 		`
+// 		type DataJsonHomePageTheReasonsWhySectionTheReasonsWhy implements Node {
+// 			reason: ReasonsWhyJson
+// 		}
+// 		type dataJson implements Node {
+// 		theReasonsWhy: [DataJsonHomePageTheReasonsWhySectionTheReasonsWhy]
+// 		}
+// 		`,
+// 	]
+// 	createTypes(typeDefs)
+// }
+
+// exports.createResolvers = ({ createResolvers }) => {
+// 	const resolvers = {
+// 		dataJson: {
+// 			theReasonsWhy: {
+// 				resolve: (source, args, context, info) => {
+// 					if (!source.theReasonsWhy) {
+// 						return undefined
+// 					}
+// 					return source.theReasonsWhy.map(reasonItem => {
+// 						const matchingReason = context.nodeModel
+// 							.getAllNodes({ type: "ReasonsWhyJson" })
+// 							.find(reason => reason.id === reasonItem.reason)
+// 						console.log("matching reasos : ", matchingReason)
+
+// 						return { matchingReason }
+// 					})
+// 				},
+// 			},
+// 		},
+// 	}
+// 	createResolvers(resolvers)
+// }
