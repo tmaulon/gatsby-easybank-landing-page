29d7865c0b2efa77eb0b540cea2a87114d7fd87e
tmaulon <thomas.maulon@gmail.com>
2020-04-22 14:07:40 +0200

    create other pages

---
+++ src/pages/support.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Layout } from "../components/layout"
+import { SEO } from "../components/seo"
+
+const SupportPage = () => (
+	<Layout>
+		<SEO title="Support page" />
+		<h1>Support page</h1>
+	</Layout>
+)
+
+export default SupportPage
